dd909975a2e67017926bf17e5767790de672775c
Tiffani Fox <tiffanipack@Tiffanipack.local>
2017-09-20 17:51:49 -0700

    Books and Authors Asssignment for Django

--- apps/Ninjas_app/admin.py
+++ apps/Ninjas_app/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Dojo, Ninja
+from .models import Dojo, Ninja, Book, Author
 admin.site.register(Dojo)
 admin.site.register(Ninja)
+admin.site.register(Book)
+admin.site.register(Author)
--- apps/Ninjas_app/models.py
+++ apps/Ninjas_app/models.py
@@ -12,3 +12,18 @@ class Ninja(models.Model):
     first_name=models.CharField(max_length=255)
     last_name=models.CharField(max_length=255)
     dojo = models.ForeignKey(Dojo, related_name="dojo_id")
+class Book(models.Model):
+    name=models.CharField(max_length=255)
+    desc=models.TextField(max_length=1000)
+    created_at=models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+class Author(models.Model):
+    first_name=models.CharField(max_length=255)
+    last_name=models.CharField(max_length=255)
+    email=models.CharField(max_length=255)
+    created_at=models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    notes=models.TextField(max_length=1000, default=0)
+    books=models.ManyToManyField(Book, related_name="authors")
+
+    
